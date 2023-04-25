3ef88bd9a91fdd9f2730daf52162897ce6cd87a8
Mirondia <mirondiakevin@gmail.com>
2023-04-25 02:39:43 -0700

    Add sorting algorithms advanced tasks

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,8 +2,8 @@
 
 /**
  * swap_nums - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * @x: The first integer to swap.
+ * @y: The second integer to swap.
  */
 void swap_nums(int *x, int *y)
 {
